1200743396190f8fa0c9e4284bce897badceb9f3
Carol Musyoka <carolmusyoka127@gmail.com>
2022-08-26 19:00:57 +0300

    tests

--- app/src/main/java/com/carolmusyoka/gitapp/data/local/Bookmarks.kt
+++ app/src/main/java/com/carolmusyoka/gitapp/data/local/Bookmarks.kt
@@ -5,3 +5,5 @@ import com.carolmusyoka.gitapp.data.model.GetUserResponse
 
 @Entity(tableName = "bookmarks_table", inheritSuperIndices = true)
 class Bookmarks: GetUserResponse()
+
+// to call Bookmars, use Bookmarks.getUserResponse()
